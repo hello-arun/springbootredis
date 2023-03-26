952208d5b3f909f44d9f552558a133a799de02ea
Arun <jangirarun786@gmail.com>
2023-03-26 20:57:09 +0300

    Revised

--- demo/src/main/java/com/springbootredis/demo/MyController.java
+++ demo/src/main/java/com/springbootredis/demo/MyController.java
@@ -1,18 +1,18 @@
 package com.springbootredis.demo;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api") // Routes
 public class MyController {
-    @GetMapping("/hello")
+    @GetMapping("/hello") // Routes
     public String hello() {
         return "Tata GoodBye See You gaya!!";
     }
     
     @GetMapping("/bye")
-        public String bye() {
+    public String bye() {
         return "Please tell me why???";
     }
 }
